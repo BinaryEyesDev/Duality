dc9bbce33f4afb2469033fde2083fc825ed75592
AmirBarak <binary.eyes.dev@gmail.com>
2022-01-27 20:52:06 +0200

    added main solution and executable project

---
+++ Source/Projects/Duality/GameDriver.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Duality
+{
+    /// <summary>
+    /// Responsible for stepping through the game's logic and rendering loops.
+    /// </summary>
+    public class GameDriver
+        : Game
+    {
+        public IGraphicsDeviceManager GraphicsDeviceManager;
+
+        protected override void Update(GameTime gameTime)
+        {
+            var keyboard = Keyboard.GetState();
+            if (keyboard.IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+    }
+}
